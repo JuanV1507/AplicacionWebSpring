8d45b72992ae3181216bf7021212945e98a43eb9
JuanDEV <Chanvazquezjuanmanuel@gmail.com>
2025-04-01 12:47:37 -0600

    Primer commit - Proyecto Spring Boot

---
+++ src/main/java/com/miapp/mi_punto/MiPuntoApplication.java
@@ -0,0 +1,13 @@
+package com.miapp.mi_punto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiPuntoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiPuntoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miapp/mi_punto/MiPuntoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miapp.mi_punto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiPuntoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
