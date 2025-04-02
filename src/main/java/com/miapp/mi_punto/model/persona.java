e69863680644df2980e54c2738b0c958a2f28911
JuanDEV <Chanvazquezjuanmanuel@gmail.com>
2025-04-02 08:21:54 -0600

    Inicio del login registro de usuario

--- src/main/java/com/miapp/mi_punto/model/persona.java
+++ src/main/java/com/miapp/mi_punto/model/persona.java
@@ -1,11 +1,13 @@
 
 package com.miapp.mi_punto.model;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 //metodos set para asignar y get obtener valor se crean las variables
 
+@Entity
 public class persona {
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
